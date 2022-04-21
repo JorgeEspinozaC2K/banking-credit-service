98286f81d3ce43be6c2cdd5fc699b18bb834aaa0
Jorge Andres Espinoza Canales <jorge.espinoza.c22@gmail.com>
2022-04-21 17:38:59 -0500

    updating

--- src/main/java/com/banking/credit/service/app/controller/CreditController.java
+++ src/main/java/com/banking/credit/service/app/controller/CreditController.java
@@ -18,7 +18,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("api/v1/credit")
+@RequestMapping("/credit")
 public class CreditController {
 	
 	@Autowired
